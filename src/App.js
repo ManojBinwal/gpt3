e0e197d5f1f52ebac83c371e87e2c1b11ca7ca50
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-04 03:09:20 +0530

    added container folder and basic files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Article , Brand , Cta , Feature, Navbar } from './components';
+import {Blog , Features, Footer , Header , Possibility , WhatGPT3 } from './containers';
 
 const App = () => {
   return (
